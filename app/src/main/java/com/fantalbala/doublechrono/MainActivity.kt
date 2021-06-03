afdc76b747fdd8d414518d6d20c877ab96632478
Emilie <fantalbala.ronflex@gmail.com>
2021-06-03 10:09:52 +0200

    feat(SB-5): update design : hide status bar, landscape and text more higther

--- app/src/main/java/com/fantalbala/doublechrono/MainActivity.kt
+++ app/src/main/java/com/fantalbala/doublechrono/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.fantalbala.doublechrono
 
+import android.app.Activity
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.os.*
 import android.os.VibrationEffect.createOneShot
-import android.view.KeyEvent
-import android.view.View
+import android.view.*
 import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -39,7 +40,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
 
         redButton = findViewById<View>(R.id.redButton) as Button
         blueButton = findViewById<View>(R.id.blueButton) as Button
@@ -71,6 +74,44 @@ class MainActivity : AppCompatActivity() {
                     timerHandler.post(timerRunnable)
                 }
             }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun Activity.hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let {
+                // Default behavior is that if navigation bar is hidden, the system will "steal" touches
+                // and show it again upon user's touch. We just want the user to be able to show the
+                // navigation bar by swipe, touches are handled by custom code -> change system bar behavior.
+                // Alternative to deprecated SYSTEM_UI_FLAG_IMMERSIVE.
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_BARS_BY_SWIPE
+                // Finally, hide the system bars, alternative to View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                // and SYSTEM_UI_FLAG_FULLSCREEN.
+                it.hide(WindowInsets.Type.systemBars())
+            }
+        } else {
+            // Enables regular immersive mode.
+            // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+            // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    // Do not let system steal touches for showing the navigation bar
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            // Hide the nav bar and status bar
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            // Keep the app content behind the bars even if user swipes them up
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            // make navbar translucent - do this already in hideSystemUI() so that the bar
+            // is translucent if user swipes it up
+            @Suppress("DEPRECATION")
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
         }
     }
 
