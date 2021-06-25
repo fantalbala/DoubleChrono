2925d93d2be26c78267cb12c19c6aaa8aa2dcbd0
Quentin EONO <quentin.eono@niji.fr>
2021-06-25 18:30:44 +0200

    feat(SB-7) : improve fullscreen handling and remove white borders

--- app/src/main/java/com/fantalbala/doublechrono/MainActivity.kt
+++ app/src/main/java/com/fantalbala/doublechrono/MainActivity.kt
@@ -40,9 +40,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setAsFullScreen()
 
         setContentView(R.layout.activity_main)
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
 
         redButton = findViewById<View>(R.id.redButton) as Button
         blueButton = findViewById<View>(R.id.blueButton) as Button
@@ -77,41 +77,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
+    private fun setAsFullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
 
-    private fun Activity.hideSystemUI() {
+        @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.let {
-                // Default behavior is that if navigation bar is hidden, the system will "steal" touches
-                // and show it again upon user's touch. We just want the user to be able to show the
-                // navigation bar by swipe, touches are handled by custom code -> change system bar behavior.
-                // Alternative to deprecated SYSTEM_UI_FLAG_IMMERSIVE.
-                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_BARS_BY_SWIPE
-                // Finally, hide the system bars, alternative to View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                // and SYSTEM_UI_FLAG_FULLSCREEN.
-                it.hide(WindowInsets.Type.systemBars())
-            }
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
-            // Enables regular immersive mode.
-            // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-            // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (
-                    // Do not let system steal touches for showing the navigation bar
-                    View.SYSTEM_UI_FLAG_IMMERSIVE
-                            // Hide the nav bar and status bar
-                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_FULLSCREEN
-                            // Keep the app content behind the bars even if user swipes them up
-                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-            // make navbar translucent - do this already in hideSystemUI() so that the bar
-            // is translucent if user swipes it up
-            @Suppress("DEPRECATION")
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
         }
     }
 
